1ce567b90ad7522a1e7abec35f7213a8475d793f
CPTWEX012 <62343020+CPTWEX012@users.noreply.github.com>
2020-07-08 09:57:56 +0200

    Update Capaciti Module 6 Week 1 Day 5 Sprint.py

--- Capaciti Module 6 Week 1 Day 5 Sprint.py
+++ Capaciti Module 6 Week 1 Day 5 Sprint.py
@@ -1,20 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
+#import pandas
 import pandas as pd
+#impoting dataframe from pandas
 from pandas import DataFrame
-d=(['Simba','Lays'],['Coke','Fanta'],['Cadbury','Tex'],['Pepper Steak','Chicken'],['Pear','Apple','Orange'],['Vanilla','Chocolate'],['Spinach','Cabbage'])
+#Declaring 2d lists
+data1=(['Simba','Lays'],['Coke','Fanta'],['Cadbury','Tex'],['Pepper Steak','Chicken'],['Pear','Apple','Orange'],['Vanilla','Chocolate'],['Spinach','Cabbage'])
+data2=(['Simba','Coke','Cadbury','Pepper Steak','Pear','Vanilla','Spinach'],['Lays','Fanta','Tex','Chicken','Apple','Chocolate','Cabbage'],['Orange'])
+#Creating list with row headings
 row=['Chips','Cooldrinks','Chocolates','Pies','Fruit','Cupcakes','Veggies']
+#Declaring list with column headings
 headers=['Product 1','Product 2','Product 3']
-df=DataFrame(data=d,index=row,columns=headers)
+#Declaring dataframes
+df=DataFrame(data=data1,index=row,columns=headers)
+df2=DataFrame(data=data2,index=headers,columns=row)
+#Displaying dataframes
 print(df)
+print('')
+print(df2)
 
-
-
-# In[ ]:
 
 
 
